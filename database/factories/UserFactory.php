dde00d42f68114e7813459694fdcdbe60b46ce3b
Kubiiz <davis953@inbox.lv>
2024-04-26 14:05:49 +0300

    Fixed UserFactory and php unit file

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'birth_date' => now()->subYears(18),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
